ec22a17a1362e4a62c58ef6b1e5ecacaf15f833b
Thomas Dekeyser <thomas@polymerlabs.org>
2024-04-09 14:30:34 +0200

    poc with initial data

---
+++ appMap.js
@@ -0,0 +1,7 @@
+export const appMap = {
+    '0x1234567890AbCdEf1234567890aBcDeF12345678' : {
+        name: 'DummyApp',
+        icon: 'https://dummyapp.com/icon.png',
+        url: 'https://dummyapp.com'
+    }
+}
---
+++ clientMap.js
@@ -0,0 +1,7 @@
+export const clientMap = {
+    'sim-test-0-0' : '11155420',
+    'sim-test-2-2' : '84532',
+    'optimism-4-4' : '11155420',
+    'optimism-5-5' : '84532',
+    'sim-test-8-8' : '49483',
+};
