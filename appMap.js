1622cd94bf9bb676871b35efdabfa29435cfdf74
mvpoyatt <mpoyatt@gmail.com>
2024-10-02 13:18:25 -0400

    Reformatted repo

--- appMap.js
+++
@@ -1,8 +0,0 @@
-// Description: This file contains a dummy mapping of app address to app name, icon and url.
-export const appMap = {
-    '0x1234567890AbCdEf1234567890aBcDeF12345678' : {
-        name: 'DummyApp',
-        icon: 'https://dummyapp.com/icon.png',
-        url: 'https://dummyapp.com'
-    }
-}
